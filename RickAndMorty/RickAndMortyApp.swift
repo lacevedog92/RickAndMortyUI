50a755b7fb273e58faa9bcd1198bd8069aa632e0
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-09-02 12:01:47 -0500

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Luis Ezcurdia on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
